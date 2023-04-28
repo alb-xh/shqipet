888f456a60852c39aec47544c0122703be5849eb
alb_xh <albano.xhafaj10@gmail.com>
2023-04-28 11:59:21 +0200

    fix: make logo non-selectable

--- apps/frontend/src/app/common/logo.component.tsx
+++ apps/frontend/src/app/common/logo.component.tsx
@@ -2,6 +2,6 @@ import logo from '../../assets/logo.png';
 
 export default function Logo() {
   return (
-    <img height={100} src={logo} alt="logo" />
+    <img height={100} src={logo} alt="logo" style={{ userSelect: "none" }} />
   );
 }
