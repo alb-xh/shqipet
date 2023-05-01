aad8a1a5e83a476033fcd1e1cbbb48a31d49db78
alb_xh <albano.xhafaj10@gmail.com>
2023-05-01 13:11:14 +0200

    feature: disable login

--- apps/web/src/app/main/index.tsx
+++ apps/web/src/app/main/index.tsx
@@ -1,13 +1,17 @@
+import { useContext } from 'react';
 import Earth from './earth.component';
 import GroupChat from './group-chat.component';
 
 import { pageStyle } from './styles';
+import UserContext from '../common/user.context';
 
 function MainPage() {
+  const { user } = useContext(UserContext);
+
   return (
     <div style={pageStyle}>
       <Earth />
-      <GroupChat />
+      { user ? <GroupChat /> : null }
     </div>
   );
 }
