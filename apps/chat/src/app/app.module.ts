be8a4cfe2f7df77661e232d299a621f5aef178dc
alb_xh <albano.xhafaj10@gmail.com>
2023-05-05 11:47:53 +0200

    feature: added missing modules from chat app

--- apps/chat/src/app/app.module.ts
+++ apps/chat/src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GeoModule } from '@shqipet/geo';
+
 import { GeoMap } from './geo.map';
 import { ChatGateway } from './chat.gateway';
 
 @Module({
+  imports: [ ConfigModule, GeoModule ],
   providers: [ GeoMap, ChatGateway ],
 })
 export class AppModule {}
