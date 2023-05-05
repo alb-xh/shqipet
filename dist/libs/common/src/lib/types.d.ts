052507e86952d8bd43501189714962cee7511ff7
alb_xh <albano.xhafaj10@gmail.com>
2023-05-05 13:46:38 +0200

    feature: attempt to deploy chat server

---
+++ dist/libs/common/src/lib/types.d.ts
@@ -0,0 +1,16 @@
+export declare enum ChatEvent {
+    UpdateGeoMap = "update_geo_map",
+    NewMessage = "new_message"
+}
+export interface GeoInfo {
+    name?: string;
+    code?: string;
+    city?: string;
+    lat?: number;
+    lng?: number;
+}
+export type GeoMap = Record<string, GeoInfo>;
+export interface UserInfo {
+    avatar?: string;
+    name: string;
+}
---
+++ dist/libs/common/src/lib/types.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ChatEvent = void 0;
+var ChatEvent;
+(function (ChatEvent) {
+    ChatEvent["UpdateGeoMap"] = "update_geo_map";
+    ChatEvent["NewMessage"] = "new_message";
+})(ChatEvent = exports.ChatEvent || (exports.ChatEvent = {}));
+;
+;
+//# sourceMappingURL=types.js.map
